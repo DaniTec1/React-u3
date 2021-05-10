e908e62dafb34b4c135c17898bebf4cf769cbab9
Dani-Rosas753 <64808943+Dani-Rosas753@users.noreply.github.com>
2021-05-10 15:16:41 -0500

    unidad 3 react, dataset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactHTMLTableToExcel from "react-html-table-to-excel";
 import DataTable from "react-data-table-component";
 import DataTableExtensions from "react-data-table-component-extensions";
 import "react-data-table-component-extensions/dist/index.css";
